28c0bf3a51b59d4b8c2a7e2f1af5484863396400
amineennaciri <spitlight.enn@gmail.com>
2023-12-16 18:27:15 +0100

    update

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {PORT,mongoDBURL} from "./config.js"
+import mongoose from "mongoose"
+import {Book} from "./models/bookModels.js"
+import booksRoute from "./routes/booksRoute.js";
+const app = express();
+
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS policy
+// Option 1: Allow all Origins with Default of cors(*);
+//app.use(cors());
+// Option 2: Allow custom Origins;
+app.use(cors({
+    origins:'http:localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type'],
+}));
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
+})
+
+app.use('/books',booksRoute);
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log(`App connected to the database`);
+    app.listen(PORT, ()=>{
+        console.log(`App is listening to port:${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(error)
+});
