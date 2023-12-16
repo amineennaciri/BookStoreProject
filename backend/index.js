102391e2b0a98ddcd7b6762ea66f5ad1651db17c
amineennaciri <spitlight.enn@gmail.com>
2023-12-16 20:34:15 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import {PORT,mongoDBURL} from "./config.js"
 import mongoose from "mongoose"
 import {Book} from "./models/bookModels.js"
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 const app = express();
 
 
@@ -11,13 +12,13 @@ app.use(express.json());
 
 // Middleware for handling CORS policy
 // Option 1: Allow all Origins with Default of cors(*);
-//app.use(cors());
+app.use(cors());
 // Option 2: Allow custom Origins;
-app.use(cors({
+/* app.use(cors({
     origins:'http:localhost:3000',
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:['Content-Type'],
-}));
+})); */
 app.get('/',(request,response)=>{
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial');
---
+++ frontend/src/pages/ShowBooks.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+const ShowBooks = () => {
+    return (
+        <div>ShowBooks</div>
+    )
+}
+
+export default ShowBooks;
